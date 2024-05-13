923a92657fadfe19d944e7d44d1227acf8e7091c
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-05-13 01:24:05 +0100

    Updated  with new content in 123

--- app/components/about/About.jsx
+++ app/components/about/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from './about.module.css'
+import Image from 'next/image'
+import img from '/public/devops.jpg'
 
 const About = () => {
   return (
-    <div className={styles.page_container}>
-      Home
+    <div className={styles.page_container} >
+      <div className={styles.about_container} id='about'>
+        <div className={styles.img}>
+          <Image className={styles.image} src={img} height={250} />
+        </div>
+        <div className={styles.description}>
+          <div className={styles.description_title1}>ABOUT ME</div>
+          <div className={styles.description_title2}>A dedicated Full-Stack Developper</div>
+          <div className={styles.description_text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, explicabo tenetur quas dolorum facilis consequatur, molestiae incidunt ex adipisci est nostrum veniam nam, rem magni recusandae repudiandae similique ratione fugit.</div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -6,10 +6,10 @@ const NavBar = () => {
     <div className={styles.navbar}>
       <div className={styles.logo}>AzizDev216</div>
       <div className={styles.nav_links}>
-        <div className={styles.nav_link}>Home</div>
-        <div className={styles.nav_link}>About</div>
-        <div className={styles.nav_link}>Skills</div>
-        <div className={styles.nav_link}>Contact</div>
+        <div className={styles.nav_link}><a className={styles.a} href="#home">Home</a></div>
+        <div className={styles.nav_link}><a className={styles.a} href="#about">About</a></div>
+        <div className={styles.nav_link}><a className={styles.a} href="#skills">Skills</a></div>
+        <div className={styles.nav_link}><a className={styles.a} href="#home">Home</a></div>
       </div>
       <div className={styles.mobile_menu}>
         <svg className={styles.menu_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
