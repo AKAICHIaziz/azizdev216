e32ffe24ace23864fdfbd7244d853dcdfc958a56
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-05-14 08:44:00 +0100

    commited on 843

--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -1,17 +1,38 @@
+"use client"
 import React from 'react'
 import styles from './home.module.css'
 import Image from 'next/image'
 import Me from '/public/me.jpg'
 import Skils from './Skils'
+import { useTypewriter, Cursor, Typewriter } from 'react-simple-typewriter'
 
-const HomeComponent = () => {  
+const HomeComponent = () => {
+
+  const { text } = useTypewriter({
+    words: ['Full-Stack Developper', 'Graphic Designer'],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 60,
+  })
+
   return (
     <div className={styles.page_container} id='home'>
       <div className={styles.profile_container}>
         <div className={styles.text}>
           <div className={styles.hi}>Hi👋</div>
           <div className={styles.name}>I'am Akaichi Med Aziz</div>
-          <div className={styles.desc}>A <span className={styles.span}>Full Stack Developper</span></div>
+          <div className={styles.desc}>A <span className={styles.span}>
+            <Typewriter
+              words={['Full-Stack Developper', 'Graphic Designer']}
+              loop={30}
+              cursor
+              cursorStyle='|'
+              typeSpeed={70}
+              deleteSpeed={40}
+              delaySpeed={1000}
+            />
+          </span>
+          </div>
           <div className={styles.desc2}>I am Akaichi Med Aziz, a full stack developper based in Tunisia.</div>
           <div className={styles.social_links}>
             <svg className={styles.social_link} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" /></svg>
